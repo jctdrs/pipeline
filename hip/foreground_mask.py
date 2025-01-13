68f591d4b244fc0928baa18e88ec517fc6bd555b
Jean Tedros <jtedros@iram.es>
2025-01-13 16:27:26 +0100

    Clean foreground_mask + fix lint

--- hip/foreground_mask.py
+++ hip/foreground_mask.py
@@ -1,14 +1,11 @@
 from typing import Optional
 
-import copy
-
 from astroquery.vizier import Vizier
 
 from astropy.wcs import WCS
 import astropy.units as au
 import astropy
 
-# TODO: Remove numpy from here
 import numpy as jnp
 
 import pyregion
@@ -70,123 +67,125 @@ class ForegroundMask(ForegroundMaskSingleton):
         astropy.io.fits.hdu.image.PrimaryHDU,
         Optional[astropy.io.fits.hdu.image.PrimaryHDU],
     ]:
-        # generate copy to be masked
+        # Generate copy to be masked
         px_size = read.pixel_size_arcsec(self.data_hdu.header)
-
-        # iterate over the fgs and mask them
         wcs = WCS(self.data_hdu.header)
 
         fgs_list, rad_fac = self.find_fgs()
 
-        # Exlude the central regions of the galaxy from the subtraction
+        # Exclude the central regions of the galaxy from the subtraction
         mask_gal_reg = self.get_mask_source()
 
-        for k in range(len(fgs_list)):
-            # convert wcs to pixel
-            fgs_pos_px = wcs.all_world2pix(fgs_list[k], 0)
-            r_mask = (
+        # Precompute the radius scaling factors for all FGS sources
+        r_masks = [
+            (
                 rad_fac[k]
                 * self.task.parameters.factor
                 * self.instruments[self.band.name]["resolutionArcsec"]
-            ) // 2
+            )
+            // 2
+            for k in range(len(fgs_list))
+        ]
 
-            # iterate over the indicidual points
+        # Create the mask for foreground sources
+        for k, fgs in enumerate(fgs_list):
+            # Convert world coordinates to pixel coordinates
+            fgs_pos_px = wcs.all_world2pix(fgs, 0)
+            r_mask = r_masks[k]
+
+            # Iterate over the points and apply the mask
             for i in range(len(fgs_pos_px)):
                 x = jnp.ceil(fgs_pos_px[i][0]).astype(int)
                 y = jnp.ceil(fgs_pos_px[i][1]).astype(int)
 
+                # Check if the pixel is within bounds and not part of the galaxy region
                 if (
                     x < self.data_hdu.header["NAXIS1"]
                     and y < self.data_hdu.header["NAXIS2"]
+                    and not mask_gal_reg[y, x]
                 ):
-                    if not mask_gal_reg[y, x]:
-                        r_circle = int(jnp.ceil(r_mask / px_size))
-                        region = """
-                             image
-                             circle({},{},{})
-                             """.format(fgs_pos_px[i][0], fgs_pos_px[i][1], r_circle)
-                        r = pyregion.parse(region)
-                        mask_fgs_reg = r.get_mask(hdu=self.data_hdu)
+                    # Create the circular mask region
+                    r_circle = int(jnp.ceil(r_mask / px_size))
+                    region = f"""
+                        image
+                        circle({fgs_pos_px[i][0]},{fgs_pos_px[i][1]},{r_circle})
+                    """
+                    r = pyregion.parse(region)
+                    mask_fgs_reg = r.get_mask(hdu=self.data_hdu)
 
-                        self.data_hdu.data[mask_fgs_reg] = jnp.nan
+                    # Apply the mask by setting the corresponding pixels to NaN
+                    self.data_hdu.data[mask_fgs_reg] = jnp.nan
 
         return self.data_hdu, self.err_hdu
 
-    # TODO: Rewrite this
-    def find_fgs(self):
+    def find_fgs(self) -> tuple[list, list]:
         mag = ["<13.5", "<14.", "<15.5", "<16.", "<18.", "<40."]
         rad_fac = [4.6, 3.0, 2.1, 1.4, 1.15, 0.7]
-        fgs1, fgs2, fgs3, fgs4, fgs5, fgs6 = [], [], [], [], [], []
-        fgs_list = [fgs1, fgs2, fgs3, fgs4, fgs5, fgs6]
 
         sizeTrim = (
             self.task.parameters.decTrim * au.arcmin,
             self.task.parameters.raTrim * au.arcmin,
         )
 
-        for k in range(len(fgs_list)):
-            # Vizier query for point sources (Gaia Data Release 3 catalog)
+        fgs_set = set()
+        fgs_list = []
+
+        for k in range(len(mag)):
             v = Vizier(
                 columns=["RAJ2000", "DEJ2000"],
                 catalog="I/355",
                 row_limit=10000,
                 column_filters={"Gmag": mag[k]},
             )
-            # look for point sources over a 4 times area with respect the cutout
-            # (2*sizeTrim)
+            # Query the region for the current magnitude bin
             fgs_table = v.query_region(
                 self.data.body, width=max(sizeTrim[0] * 2, sizeTrim[1] * 2)
             )
 
             fgs_sources = fgs_table[0]
-            fgs_skycoord = [fgs_sources["RAJ2000"], fgs_sources["DEJ2000"]]
-            fgs_RA = jnp.asarray(fgs_skycoord[0:][0])
-            fgs_DEC = jnp.asarray(fgs_skycoord[1:][0])
+            fgs_RA, fgs_DEC = (
+                jnp.asarray(fgs_sources["RAJ2000"]),
+                jnp.asarray(fgs_sources["DEJ2000"]),
+            )
             fgs = jnp.vstack([fgs_RA, fgs_DEC]).T
 
-            if k == 0:
-                fgs_list[k] = copy.deepcopy(fgs)
+            fgs_unique = [tuple(elem) for elem in fgs if tuple(elem) not in fgs_set]
 
-            else:
-                fgs_temp = jnp.empty((0, 2))
-                for j in range(k):
-                    fgs_temp = jnp.append(fgs_temp, fgs_list[j], axis=0)
-                fgs_k = jnp.empty((0, 2))
-                for elem in fgs:
-                    if elem not in fgs_temp:
-                        fgs_k = jnp.append(
-                            fgs_k, jnp.array([[elem[0], elem[1]]]), axis=0
-                        )
-                fgs_list[k] = copy.deepcopy(fgs_k)
+            # Add unique sources to the set and the list
+            for elem in fgs_unique:
+                fgs_set.add(elem)
+
+            fgs_list.append(jnp.array(fgs_unique))
 
         return fgs_list, rad_fac
 
-    def get_mask_source(self):
+    def get_mask_source(self) -> jnp.ndarray:
         px_size = read.pixel_size_arcsec(self.data_hdu.header)
 
         wcs = WCS(self.data_hdu.header)
 
-        pos_ctr = jnp.vstack([(self.data.geometry.ra), self.data.geometry.dec]).T
-        pos_center_px = wcs.all_world2pix(pos_ctr, 0)
-        rma_gal = self.data.geometry.semiMajorAxis / 2
-        rmi_gal = rma_gal / self.data.geometry.axialRatio
-        rma_gal_px = jnp.ceil(rma_gal / px_size)
-        rmi_gal_px = jnp.ceil(rmi_gal / px_size)
+        position_px = wcs.all_world2pix(
+            self.data.geometry.ra, self.data.geometry.dec, 0
+        )
+        rma_ = self.data.geometry.semiMajorAxis / 2
+        rmi_ = rma_ / self.data.geometry.axialRatio
+
+        rma = int(jnp.ceil(rma_ / px_size))
+        rmi = int(jnp.ceil(rmi_ / px_size))
 
         region = """
                 image
                 ellipse({},{},{},{},{})
                 """.format(
-            pos_center_px[0][0],
-            pos_center_px[0][1],
-            rma_gal_px,
-            rmi_gal_px,
+            position_px[0],
+            position_px[1],
+            rma,
+            rmi,
             self.data.geometry.positionAngle,
         )
 
         r = pyregion.parse(region)
         mask_reg = r.get_mask(hdu=self.data_hdu)
-
         return mask_reg
 
 
