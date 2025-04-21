e1d3e06b7d428bb5e37bbed6a9b0a51e11ca0135
Jean Tedros <jtedros@iram.es>
2025-04-21 16:14:59 +0200

    Add Gitlab CI configuration

---
+++ tests/test_e2e.py
@@ -0,0 +1,10 @@
+import subprocess
+
+
+def test_NGC2454_pipeline():
+    result = subprocess.run(
+        ["python", "main.py", "-f", "pipeline.yml"],
+        check=True,
+        capture_output=True,
+    )
+    assert "Integrated flux PACS1 = 62.654 Jy/px" in result.stdout.decode("utf-8")
