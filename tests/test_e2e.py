b19badcec7ce399acf47fd79d3d40ea1a75fe306
Jean Tedros <jtedros@iram.es>
2025-04-21 17:23:24 +0200

    Directories for testing

--- tests/test_e2e.py
+++ tests/test_e2e.py
@@ -3,12 +3,8 @@ import subprocess
 
 def test_NGC2454_pipeline():
     result = subprocess.run(
-        ["python", "main.py", "-f", "tests/pipeline_test.yml"],
+        ["python", "main.py", "-f", "tests/pipelines/pipeline_test.yml"],
         check=True,
         capture_output=True,
     )
-    try:
-        assert "Integrated flux PACS1 = 62.654 Jy/px" in result.stdout.decode("utf-8")
-    except AssertionError:
-        print(result.stderr.decode("utf-8"))
-        assert False
+    assert "Integrated flux PACS1 = 62.654 Jy/px" in result.stdout.decode("utf-8")
